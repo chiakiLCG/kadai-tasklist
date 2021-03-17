be4feb604efebba10ad3642a08bcf28ac0eb7919
chiaki.ueno <s1c2r6@gmail.com>
2021-03-17 12:57:44 +0000

    user login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,10 @@
 class UsersController < ApplicationController
+  before_action :require_user_logged_in, only: [:index, :show]
   
   def new
     @user = User.new
   end
-
+  
   def create
     @user = User.new(user_params)
 
@@ -14,6 +15,10 @@ class UsersController < ApplicationController
       flash.now[:danger] = 'ユーザの登録に失敗しました。'
       render :new
     end
+  end
+  
+  def show
+    @user = User.find(params[:id])
   end
   
   private
